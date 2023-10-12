df76491ccb538d6f5402b92c2259a03db0dabe66
faritg1 <113639494+faritg1@users.noreply.github.com>
2023-10-12 13:05:56 -0500

    feat: :hammer: Implementando Dbset de las entities

--- Infrastructure/Data/NotificacionContext.cs
+++ Infrastructure/Data/NotificacionContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
@@ -11,6 +12,15 @@ public class NotificacionContext : DbContext
     protected NotificacionContext(DbContextOptions options) : base(options)
     {
     }
+    public DbSet<Auditoria> Auditorias { get; set; }
+    public DbSet<Blockchain> Blockchains { get; set; }
+    public DbSet<EstadoNotificacion> EstadosNotificaciones { get; set; }
+    public DbSet<Formato> Formatos { get; set; }
+    public DbSet<HiloRespuestaNotificacion> HilosRespuestasNotificaciones { get; set; }
+    public DbSet<ModuloNotificacion> ModulosNotificaciones { get; set; }
+    public DbSet<Radicado> Radicados { get; set; }
+    public DbSet<TipoNotificacion> TiposNotificaciones { get; set; }
+    public DbSet<TipoRequerimiento> TipoRequerimientos { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {

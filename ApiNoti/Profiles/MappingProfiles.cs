e856893ce254d9d090a6e03f10f666447ec57a1a
faritg1 <113639494+faritg1@users.noreply.github.com>
2023-10-17 11:50:36 -0500

    feat: :hammer: Controller auditoria y mapper

--- ApiNoti/Profiles/MappingProfiles.cs
+++ ApiNoti/Profiles/MappingProfiles.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiNoti.Dtos;
 using AutoMapper;
+using Core.Entities;
 
 namespace ApiNoti.Profiles
 {
     public class MappingProfiles : Profile
     {
         public MappingProfiles() {
-            
+            CreateMap<Auditoria, AuditoriaDto>().ReverseMap();
         }
     }
 }
